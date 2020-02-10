6b64efe087aa9fe963dc61babeef64619aef7063
Halshubaili <55226417+Halshubaili@users.noreply.github.com>
2020-02-10 21:21:43 +0300

    after merging, + empty post bid

---
+++ app/src/main/java/com/example/seeker/PostBid/PostBidActivity.java
@@ -0,0 +1,16 @@
+package com.example.seeker.PostBid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.seeker.R;
+
+public class PostBidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_bid);
+    }
+}
