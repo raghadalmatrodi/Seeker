948652d8260d60a05038bf207d900b15e8ca1656
raghad <raghadalmatrodi@outlook.com>
2020-02-19 00:43:17 +0300

    Create Freelancer_SearchFragment.java

---
+++ app/src/main/java/com/example/seeker/FreelancerMainPages/SearchTab/Freelancer_SearchFragment.java
@@ -0,0 +1,20 @@
+package com.example.seeker.FreelancerMainPages.SearchTab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.seeker.R;
+
+
+public class Freelancer_SearchFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_freelancer_search, container, false);
+    }
+}
