2f873227666c6cde512d9cb06351492f38590c8c
Noura <NouraAlsabr@hotmail.com>
2020-04-19 20:34:35 +0300

    Token for notification fixed :) please delete the app from the emulator and reRun

--- app/src/main/java/com/example/seeker/Activities/SplashActivity.java
+++ app/src/main/java/com/example/seeker/Activities/SplashActivity.java
@@ -6,16 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.util.Log;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.seeker.ApplictionNAme;
+import com.example.seeker.Database.ApiClients;
 import com.example.seeker.EmployerMainPages.EmployerMainActivity;
 import com.example.seeker.FreelancerMainPages.FreelancerMainActivity;
 import com.example.seeker.Model.Freelancer;
+import com.example.seeker.Model.Responses.ApiResponse;
 import com.example.seeker.R;
 import com.example.seeker.SharedPref.Constants;
 import com.example.seeker.SharedPref.MySharedPreference;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SplashActivity extends Activity {
 
@@ -47,7 +54,7 @@ public class SplashActivity extends Activity {
         if (MySharedPreference.getBoolean(this, Constants.Keys.IS_LOGIN, false)) {
 
 
-
+            executeUpdateToken();
 
 
             if(MySharedPreference.getString(SplashActivity.this,Constants.Keys.USER_CURRENT_TYPE,"0").equals("EMPLOYER")){
@@ -65,6 +72,33 @@ public class SplashActivity extends Activity {
             finish();
         }//End of else
     }//End of checkIsLogin
+
+    private void executeUpdateToken() {
+
+        String token = MySharedPreference.getString(getApplicationContext(),Constants.Keys.TOKEN_ID,"");
+        Long userId = MySharedPreference.getLong(getApplicationContext(),Constants.Keys.USER_ID,-1);
+
+        if(!((token == null || token.equals("")) ) && userId !=-1 ){
+            ApiClients.getAPIs().updateToken(token ,userId ).enqueue(new Callback<ApiResponse>() {
+                @Override
+                public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                    if(response.isSuccessful()){
+                      Log.d(SplashActivity.class.getSimpleName(), "successful token: " + response.message());
+
+                    }else{
+                      Log.d(SplashActivity.class.getSimpleName(), "Not suc: " + response.message());
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ApiResponse> call, Throwable t) {
+                    Log.d(SplashActivity.class.getSimpleName(), "Failure : " + t.getMessage());
+
+                }
+            });
+        }
+    }
 
 
 }//End class
